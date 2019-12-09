6af65c82e93210f97e272622ddb8d15b1aa01361
StarCloud <warcnx@gmail.com>
2019-12-09 11:09:37 +0800

    91

--- Script/Wechat.js
+++ Script/Wechat.js
@@ -1,14 +1,19 @@
-/**
- * @supported 23AD6B11 55BE3B10 63ED6F86
- */
 /*
-[rewrite_local]
-^https?://mp.weixin.qq.com/mp/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+Remove the WeChat public account bottom ad
 
-hostname = mp.weixin.qq.com,
+QX:
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge4：
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge & QX MITM = mp.weixin.qq.com,
 */
 
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
+delete obj.appid;
 $done({body: JSON.stringify(obj)}); 
+
+//by Choler
